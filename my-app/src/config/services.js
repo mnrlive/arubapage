b8b312aa8d20b5dfe0d50d97518eba50b15fa703
Sitelift <robingiel@gmail.com>
2019-02-17 21:32:35 +0100

    using own cors server

--- my-app/src/config/services.js
+++ my-app/src/config/services.js
@@ -1,16 +1,16 @@
 export const arubaNative = 'https://wordpressmade.com/https://arubanative.com/wp-json/wp/v2/posts?_embed&per_page=12';
-export const noticiaCla = 'https://cors-anywhere.herokuapp.com/https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.noticiacla.com%2Frss';
+export const noticiaCla = 'https://wordpressmade.com/https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.noticiacla.com%2Frss';
 export const eArubianoNews = require('../images/earubiano');
 export const aweMainta = 'https://wordpressmade.com/https://awemainta.com/wp-json/wp/v2/posts?_embed&per_page=12';
-export const _24ora = 'https://cors-anywhere.herokuapp.com/https://24ora.com/wp-json/wp/v2/posts?_embed&per_page=12';
-export const boletinExtra = 'https://cors-anywhere.herokuapp.com/https://boletinextra.com/wp-json/wp/v2/posts?_embed&per_page=12';
-export const masNoticia = 'https://cors-anywhere.herokuapp.com/https://www.masnoticia.com/wp-json/wp/v2/posts?_embed&per_page=12';
+export const _24ora = 'https://wordpressmade.com/https://24ora.com/wp-json/wp/v2/posts?_embed&per_page=12';
+export const boletinExtra = 'https://wordpressmade.com/https://boletinextra.com/wp-json/wp/v2/posts?_embed&per_page=12';
+export const masNoticia = 'https://wordpressmade.com/https://www.masnoticia.com/wp-json/wp/v2/posts?_embed&per_page=12';
 export const diario = 'https://wordpressmade.com/https://www.diario.aw/wp-json/wp/v2/posts?_embed&per_page=12';
 export const focus = 'https://wordpressmade.com/https://focus.aw/index.php/wp-json/wp/v2/posts?_embed&per_page=12';
 export const batiBleki = 'https://wordpressmade.com/https://www.visitaruba.com/blog/wp-json/wp/v2/posts?_embed&per_page=12';
 export const bonDia = 'https://wordpressmade.com/https://www.bondia.com/wp-json/wp/v2/posts?_embed&per_page=12';
 export const sports = 'https://wordpressmade.com/http://www.297sports.com/wp-json//wp/v2/posts?_embed&per_page=12';
-export const solo = 'https://cors-anywhere.herokuapp.com/http://solodipueblo.com//wp-json/wp/v2/posts?_embed&per_page=12';
+export const solo = 'https://wordpressmade.com/http://solodipueblo.com//wp-json/wp/v2/posts?_embed&per_page=12';
 
 const services = {
     arubaNative,noticiaCla,eArubianoNews,aweMainta,_24ora,boletinExtra,masNoticia,diario,focus,batiBleki,bonDia,sports,solo
