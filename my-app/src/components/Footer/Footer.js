e2d378d2e3de9afdf808bf00bb4b0e31c1b70451
Robin Giel <home@Knaras-MacBook-Pro.local>
2019-09-17 01:17:36 +0200

    footer player for edge the same plus volume fix

--- my-app/src/components/Footer/Footer.js
+++ my-app/src/components/Footer/Footer.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import AudioPlayer from 'react-responsive-audio-player';
 import { playlist } from "../../utils/playlist";
-import { isEdge } from 'react-device-detect';
 import { MediaPlayer } from '@cassette/player';
 
 const Footer = () => {
   return (
     <div id="radio">
-      {
-          (isEdge) ?
-        ( <AudioPlayer playlist={playlist} />) : (<MediaPlayer playlist={playlist} controls = {['backskip', 'playpause', 'forwardskip', 'volume', 'progress']}/>)
-      }
+       <MediaPlayer playlist={playlist} controls = {['backskip', 'playpause', 'forwardskip', 'volume', 'progress']}/>
     </div>
   );
 };
