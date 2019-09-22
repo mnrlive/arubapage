491f2bd435ec81642e7f2c56e159831c0a53c6d9
Robin Giel <home@Knaras-MacBook-Pro.local>
2019-09-22 04:43:31 +0200

    no slot size build

--- my-app/src/components/GoogleAd.js
+++ my-app/src/components/GoogleAd.js
@@ -8,9 +8,10 @@ export default class GoogleAd extends Component {
         return (
             <ins className='adsbygoogle'
                 style={{ display: "block"}}
-                data-ad-client='ca-pub-8107944427019798'
-                data-ad-slot='3825049427'
-                data-ad-format='auto' />
+                data-ad-client="ca-pub-8107944427019798"
+                data-ad-slot="3825049427"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
         );
     }
 }
