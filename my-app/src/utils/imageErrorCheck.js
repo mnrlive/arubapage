ad41ab540df826351f7c68be2ceb456ea612520b
Robin Giel <home@Knaras-MacBook-Pro.local>
2020-05-25 00:46:50 +0200

    changes removed console log

--- my-app/src/utils/imageErrorCheck.js
+++ my-app/src/utils/imageErrorCheck.js
@@ -1,5 +1,5 @@
 // Cathing erros and showing default
-export const imageErrorCheck = function(provider) {
+export const imageErrorCheck = function (provider) {
   const regEx = /[-a-zA-Z0-9@:%_.~#?&=]{2,256}\.[a-z]{2,4}/;
   const link = regEx.exec(provider.link);
 
@@ -10,47 +10,52 @@ export const imageErrorCheck = function(provider) {
     } catch (e) {
       return require("../images/arubaNative.PNG");
     }
-  } else if (link[0] === "www.bondia.com") {
+  }
+  if (link[0] === "www.bondia.com") {
     try {
       return provider._embedded["wp:featuredmedia"][0].media_details.sizes.full
         .source_url;
     } catch (e) {
       return require("../images/bondia.PNG");
     }
-  } else if (link[0] === "focus.aw") {
+  }
+  if (link[0] === "focus.aw") {
     try {
       return provider._embedded["wp:featuredmedia"][0].media_details.sizes.full
         .source_url;
     } catch (e) {
       return require("../images/focus.PNG");
     }
-  } else if (link[0] === "xclusivomagazine.com") {
+  }
+  if (link[0] === "xclusivomagazine.com") {
     try {
       return provider._embedded["wp:featuredmedia"][0].media_details.sizes.full
         .source_url;
     } catch (e) {
       return require("../images/focus.PNG");
     }
-  } else if (link[0] === "www.earubianonews.com") {
+  }
+  if (link[0] === "www.earubianonews.com") {
     try {
       const regexErubiano = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg)/g;
       return regexErubiano.exec(provider.content.rendered)[0];
     } catch (e) {
       return require("../images/eArubiano.PNG");
     }
-  } else if (link[0] === "www.awemainta.com") {
+  }
+  if (link[0] === "www.awemainta.com") {
     try {
       const aweM = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg)/g;
       let initial_url;
       let processed_url;
       initial_url = aweM.exec(provider.content.rendered)[0];
       processed_url = "http" + initial_url.slice(5);
-      console.log(processed_url);
       return processed_url;
     } catch (e) {
       return require("../images/aweMainta.PNG");
     }
-  } else if (link[0] === "awe24.com") {
+  }
+  if (link[0] === "awe24.com") {
     // const regImage = /src\s*=\s*"(.+?)"/;
     const regImage = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg)/g;
     try {
@@ -58,41 +63,47 @@ export const imageErrorCheck = function(provider) {
     } catch (e) {
       return require("../images/awe.PNG");
     }
-  } else if (link[0] === "www.297sports.com") {
+  }
+  if (link[0] === "www.297sports.com") {
     try {
       return provider._embedded["wp:featuredmedia"][0].media_details.sizes.full
         .source_url;
     } catch (e) {
       return require("../images/coolFm.png");
     }
-  } else if (link[0] === "diario.aw") {
+  }
+  if (link[0] === "diario.aw") {
     try {
       return provider.jetpack_featured_media_url;
     } catch (e) {
       return require("../images/diario.PNG");
     }
-  } else if (link[0] === "masnoticia.com") {
+  }
+  if (link[0] === "masnoticia.com") {
     try {
       return provider._embedded["wp:featuredmedia"][0].media_details.sizes.full
         .source_url;
     } catch (e) {
       return require("../images/masnoticia.PNG");
     }
-  } else if (link[0] === "solodipueblo.com") {
+  }
+  if (link[0] === "solodipueblo.com") {
     try {
-        return provider._embedded["wp:featuredmedia"][0].media_details.sizes.full
+      return provider._embedded["wp:featuredmedia"][0].media_details.sizes.full
         .source_url;
     } catch (e) {
       return require("../images/soloDefaultimg.jpg");
     }
-  } else if (link[0] === "www.visitaruba.com") {
+  }
+  if (link[0] === "www.visitaruba.com") {
     try {
       return provider._embedded["wp:featuredmedia"][0].media_details.sizes.full
         .source_url;
     } catch (e) {
       return require("../images/batiBlekiHD.PNG");
     }
-  } else if (link[0] === "xclusivomagazine.com") {
+  }
+  if (link[0] === "xclusivomagazine.com") {
     try {
       return provider._embedded["wp:featuredmedia"][0].media_details.sizes.full
         .source_url;
--- my-app/src/utils/imageFunctions.js
+++ my-app/src/utils/imageFunctions.js
@@ -2,47 +2,21 @@
 // const regex = /^(.*[\\\/])(.*)/;
 // const regex2 = /[^\/]*(.*)/;
 
-export const imageRuba = function(arubiano) {
-            try {
-                return require('../images/eArubiano.PNG');
-                // return (('https://arubapage.com/static/media/' + (regex.exec(arubiano._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url)[2]) + '.1'));
-            } catch (e) {
-                return require('../images/eArubiano.PNG');
-            }
-}
-
-export const imageTest = function(boletin) {
-            try {
-                return require('../images/boletinHD.jpg');
-                // return ('https://arubapage.com/static/media/' + (regex.exec(boletin._embedded['wp:featuredmedia'][0].source_url)[2]) + '.1');
-            } catch (e) {
-                return require('../images/boletinHD.jpg');
-            }
-}
 
 export const imageBintiCuater = function (ora) {
-                    try {
-                        // return ('https://arubapage.com/static/media/' + (regex.exec(ora._embedded['wp:featuredmedia'][0].source_url)[2]) + '.1');
-                        return require('../images/24ora.jpg');
-                    } catch (e) {
-                        return require('../images/24ora.jpg');
-                    }
-}
-
-// export const imageSolo = function (solo) {
-//                     try {
-//                         return ('https://arubapage.com/static/media/' + (regex.exec(solo._embedded['wp:featuredmedia'][0].media_details['file'])[2]) + '.1');
-//                     } catch (e) {
-//                         return require('../images/boletinHD.jpg');
-//                     }
-// }
+  try {
+    // return ('https://arubapage.com/static/media/' + (regex.exec(ora._embedded['wp:featuredmedia'][0].source_url)[2]) + '.1');
+    return require("../images/24ora.jpg");
+  } catch (e) {
+    return require("../images/24ora.jpg");
+  }
+};
 
 export const imageSports = function (sports) {
-        let url = sports._embedded['wp:featuredmedia'][0].source_url;
-                    try {
-                        // return ('http:' + (regex2.exec(sports._embedded['wp:featuredmedia'][0].source_url)[1]));
-                         return url.replace(/^https:\/\//i, 'http://');
-                    } catch (e) {
-                        return require('../images/297sport.JPG');
-                    }
-}
+  let url = sports._embedded["wp:featuredmedia"][0].source_url;
+  try {
+    return url.replace(/^https:\/\//i, "http://");
+  } catch (e) {
+    return require("../images/297sport.JPG");
+  }
+};
